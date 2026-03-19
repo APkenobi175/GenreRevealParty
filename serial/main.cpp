c1a133f51401b50720a0fed879369170a5f9fcd1
CobaltGoldCS <cobaltgoldsprinks@gmail.com>
2026-03-18 21:14:39 -0600

    Initial Implementation of Serial KmeansClustering
    This is not in the correct format, and clusters a dummy csv currently. Just trying to get things going shared code wise

---
+++ serial/main.cpp
@@ -0,0 +1,35 @@
+#include "fileops.hpp"
+#include "kmeans.hpp"
+#include "points.hpp"
+
+#include <array>
+#include <vector>
+
+constexpr int CLUSTER_COUNT = 5;
+constexpr int KMEANS_ITERATIONS = 5;
+
+void kMeansClustering(std::vector<Point> &points) {
+  std::array<Point, CLUSTER_COUNT> centroids;
+  initializeCentroids(points, centroids.data(), CLUSTER_COUNT);
+
+  std::array<Point, CLUSTER_COUNT> centroid_temps;
+  std::array<int, CLUSTER_COUNT> points_per_cluster;
+  centroid_temps.fill(initialize_point(0, 0));
+  points_per_cluster.fill(0);
+  for (int i = 0; i < KMEANS_ITERATIONS; i++) {
+    partitionClusters(centroids.data(), CLUSTER_COUNT, &points[0],
+                      points.size(), centroid_temps.data(),
+                      points_per_cluster.data());
+    computeCentroids(centroids.data(), centroid_temps.data(),
+                     points_per_cluster.data(), CLUSTER_COUNT);
+    // Reset Temporary variables
+    centroid_temps.fill(initialize_point(0, 0));
+    points_per_cluster.fill(0);
+  }
+  writecsv(centroids.data(), CLUSTER_COUNT);
+}
+
+int main() {
+  std::vector<Point> points = readcsv();
+  kMeansClustering(points);
+}
