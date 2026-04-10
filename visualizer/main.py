bd411bef7261f398a544c2713ff82d3f67c72891
phippsammon <phippsammon175@gmail.com>
2026-04-10 09:01:39 -0600

    Changed vizualizer a bit, pushing so I can run on cluster

--- visualizer/main.py
+++ visualizer/main.py
@@ -1,20 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
 import sys
-if (len(sys.argv) < 2):
-    print("Usage: python visualize.py <file>", file=sys.stderr)
-    sys.exit(1)
 
-# After clustering
-plt.figure()
-df = pd.read_csv(sys.argv[1])
-sns.scatterplot(x=df.danceability, y=df.energy, 
-                hue=df.c, 
-                palette=sns.color_palette("hls", n_colors=5))
-plt.xlabel("danceability")
-plt.ylabel("energy")
-plt.title("Clustered: (y) vs (x)")
+def main():
+    if (len(sys.argv) < 2):
+        print("Usage: python visualize.py <file>", file=sys.stderr)
+        sys.exit(1)
 
-plt.show()
+    df = pd.read_csv(sys.argv[1])
+    features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveliness', 'valence']
+    sns.pairplot(df, hue = 'c', palette = 'hls', vars=features)
+    plt.suptitle("K-Means Clustering: Spotify Audio Features")
+    plt.show()
+    # # After clustering
+    # plt.figure()
+    # df = pd.read_csv(sys.argv[1])
+    # sns.scatterplot(x=df.danceability, y=df.energy, 
+    #                 hue=df.c, 
+    #                 palette=sns.color_palette("hls", n_colors=5))
+    # plt.xlabel("danceability")
+    # plt.ylabel("energy")
+    # plt.title("Clustered: (y) vs (x)")
+
+    # plt.show()
+
+if __name__ == "__main__":
+    main()
